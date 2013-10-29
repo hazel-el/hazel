1a0457cfa725503f5e47d92d14d827281adc28e9
Francesco Kriegel <francesco-kriegel@users.noreply.github.com>
2013-10-29 12:38:38 +0100

    NF4 NF5

--- Hazel/Completion.hs
+++ Hazel/Completion.hs
@@ -27,6 +27,27 @@ normalizeGCI gci = case gci of
     Subclass (And c (Exists r d)) e -> 
         (normalizeGCI $ Subclass (Exists r d) toDummy) ++
         (normalizeGCI $ Subclass (And toDummy c) e)
+        
+        
+        
+    Subclass c (Exists r d) ->
+        (normalizeGCI $ Subclass c (Exists r toDummy)) ++
+        (normalizeGCI $ Subclass toDummy d)
+    Subclass (And c d) e ->
+        (normalizeGCI $ Subclass (And c d) toDummy) ++
+        (normalizeGCI $ Subclass toDummy e)
+    Subclass (Exists r c) d ->
+        (normalizeGCI $ Subclass (Exists r c) toDummy) ++
+        (normalizeGCI $ Subclass toDummy d)
+    Subclass c (And d e) ->
+        (normalizeGCI $ Subclass c toDummy) ++
+        (normalizeGCI $ Subclass toDummy (And d e))
+    Subclass c (Exists r d) ->
+        (normalizeGCI $ Subclass c toDummy) ++
+        (normalizeGCI $ Subclass toDummy (Exists r d)) 
+    Subclass c (And d e) ->
+        (normalizeGCI $ Subclass c d) ++
+        (normalizeGCI $ Subclass c e)
     g ->
         [g]
   where
