caac4953778c85cf03b2c843b29bf31f6d4838aa
Felix <felix@tcs.inf.tu-dresden.de>
2013-10-29 16:19:57 +0100

    Auxiliary functions for TBox

--- Hazel/Core.hs
+++ Hazel/Core.hs
@@ -15,29 +15,34 @@ module Hazel.Core(
     Role (..),
     Concept (..),
     GCI (..),
-    TBox
+    TBox (..),
+    tBox_from_list,
+    tBox_union
 ) where
 
 import Data.HashTable (hashString)
--- import Data.Set
+import Data.Set
 
 
 -- Datatypes --
 
 data Role =
     Role String
-
+    deriving (Eq, Ord)
+    
 data Concept =
     Top |
     Name String Bool | -- Bool flag is true if it's not a dummy
     And Concept Concept |
     Exists Role Concept
+    deriving (Eq, Ord)
 
 data GCI =
     Subclass Concept Concept
 
-type TBox =
-    [GCI]
+data TBox =
+    -- TBox gcis concept_names role_names
+    TBox [GCI] (Set Concept) (Set Role) 
 
 
 -- show functions defined according to Manchester OWL Syntax used by Protege:
@@ -61,3 +66,47 @@ instance Show Concept where
 instance Show GCI where
     show (Subclass c1 c2) =
         (show c1) ++ " SubClassOf " ++ (show c2)
+
+instance Show TBox where
+    show (TBox gs _ _) = show gs
+
+
+-- Auxiliary functions
+
+pair_union :: Ord a => Ord b => (Set a, Set b) -> (Set a, Set b) -> (Set a, Set b)
+pair_union (sx, sy) (tx, ty) =
+    (sx `union` tx, sy `union` ty)
+
+
+get_names :: Concept -> (Set Concept, Set Role)
+get_names c = case c of
+    Top ->
+        (empty, empty)
+    And c d ->
+        get_names c `pair_union` get_names d
+    Exists r d ->
+        get_names d `pair_union` (empty, singleton r)
+    Name s b -> (singleton (Name s b), empty)
+
+
+get_names_gci :: GCI -> (Set Concept, Set Role)
+get_names_gci (Subclass c d) =
+    get_names c `pair_union` get_names d
+
+
+add_GCI :: TBox -> GCI -> TBox
+add_GCI (TBox gs cs rs) g =
+    TBox (g:gs) (cs `union` gcs) (rs `union` grs)
+  where
+    (gcs, grs) = get_names_gci g
+
+
+tBox_from_list :: [GCI] -> TBox
+tBox_from_list [] = TBox [] empty empty
+tBox_from_list (g:gt) =
+    add_GCI (tBox_from_list gt) g
+
+
+tBox_union :: TBox -> TBox -> TBox
+tBox_union (TBox gs sc sr) (TBox hs tc tr) =
+    TBox (gs ++ hs) (sc `union` tc) (sr `union` tr)
