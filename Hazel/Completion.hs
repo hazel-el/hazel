ecadf53c50c0550dc4d1e748f91e9e605aa737e3
Felix <felix@tcs.inf.tu-dresden.de>
2013-10-30 15:14:36 +0100

    Completion Rules finished

--- Hazel/Completion.hs
+++ Hazel/Completion.hs
@@ -11,62 +11,84 @@
  -
  - <module description starting at first column>
  - -}
-module Hazel.Completion where
+module Hazel.Completion
+where
 
 import Hazel.Core
--- import Data.Set
 
 
-normalizeGCI :: GCI -> TBox
-normalizeGCI gci = case gci of
-    -- Rule NF2 in Pushing the EL Envelope
-    Subclass (And c (And d1 d2)) e -> 
-        (normalizeGCI $ Subclass (And d1 d2) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (And dummy c) e)
-    Subclass (And c (Exists r d)) e -> 
-        (normalizeGCI $ Subclass (Exists r d) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (And dummy c) e)
-    -- Rule NF2 with the order of conjuncts flipped
-    Subclass (And (And d1 d2) c) e -> 
-        (normalizeGCI $ Subclass (And d1 d2) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (And dummy c) e)
-    Subclass (And (Exists r d) c) e -> 
-        (normalizeGCI $ Subclass (Exists r d) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (And dummy c) e)
-    -- Rule NF3
-    Subclass (Exists r (And c1 c2)) d ->
-        (normalizeGCI $ Subclass (And c1 c2) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (Exists r dummy) d)
-    Subclass (Exists r (Exists r1 c1)) d ->
-        (normalizeGCI $ Subclass (Exists r1 c1) dummy) `tBox_union`
-        (normalizeGCI $ Subclass (Exists r dummy) d)
-    -- Rule NF6
-    Subclass d (Exists r (And c1 c2)) ->
-        (normalizeGCI $ Subclass dummy (And c1 c2)) `tBox_union`
-        (normalizeGCI $ Subclass d (Exists r dummy))
-    Subclass c (Exists r (Exists s d)) ->
-        (normalizeGCI $ Subclass c (Exists r dummy)) `tBox_union`
-        (normalizeGCI $ Subclass dummy (Exists s d))
-    -- Rule NF5
-    Subclass (And c d) (Exists re e) ->
-        (normalizeGCI $ Subclass (And c d) dummy) `tBox_union`
-        (normalizeGCI $ Subclass dummy (Exists re e))
-    Subclass (Exists r c) (And d1 d2) ->
-        (normalizeGCI $ Subclass (Exists r c) dummy) `tBox_union`
-        (normalizeGCI $ Subclass dummy (And d1 d2))
-    Subclass (And c1 c2) (And d e) ->
-        (normalizeGCI $ Subclass (And c1 c2) dummy) `tBox_union`
-        (normalizeGCI $ Subclass dummy (And d e))
-    Subclass (Exists rc c) (Exists r d) ->
-        (normalizeGCI $ Subclass (Exists rc c) dummy) `tBox_union`
-        (normalizeGCI $ Subclass dummy (Exists r d))
-    -- Rule NF7
-    Subclass c (And d e) ->
-        (normalizeGCI $ Subclass c d) `tBox_union`
-        (normalizeGCI $ Subclass c e)
-    -- boring GCIs:
-    Subclass _ Top -> tBox_from_list []
-    g ->
-        tBox_from_list [g]
+data CGraph =
+    -- |   S mapping              R mapping
+    CGraph (Concept -> [Concept]) (Role -> [CEdge])
+        
+type CEdge = (Concept,Concept)
+        
+        
+
+-- Auxiliary functions
+
+-- | Returns a function that is like f, except that x is mapped to yn
+except :: (Eq a) => (a -> b) -> a -> b -> (a -> b)
+except f x yn =
+    fn
   where
-    dummy = Name (show gci) False
+    fn o
+        | o == x = yn
+        | otherwise = f o
+
+init_graph =
+    CGraph s r
+  where
+    s Top = [Top]
+    s (Name n f) = [Name n f, Top]
+    r _ = []
+
+
+-- Functions applying Completion Rules
+
+cr1 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
+cr2 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
+cr3 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
+cr4 :: GCI -> CGraph -> Concept -> Concept -> (CGraph, Bool)
+
+cr1 (Subclass c' d) (CGraph s r) c
+    | (c' `elem` s c) && not (d `elem` s c) = (CGraph sneu r, True)
+    | otherwise = (CGraph s r, False)
+  where
+    sneu = except s c (d:s c)
+
+cr2 (Subclass (And c1 c2) d) (CGraph s r) c
+    | (c1 `elem` s c) && (c2 `elem` s c) && not (d `elem` s c) =
+        (CGraph sneu r, True)
+    | otherwise = (CGraph s r, False)
+  where
+    sneu = except s c (d:s c)
+
+cr3 (Subclass c' (Exists role d)) (CGraph s r) c
+    | (c' `elem` s c) && not ((c, d) `elem` r role) =
+        (CGraph s rneu, True)
+    | otherwise = (CGraph s r, False)
+  where
+    rneu = except r role ((c, d):r role) 
+
+cr4 (Subclass (Exists role d') e) (CGraph s r) c d
+    | ((c, d) `elem` r role) && (d' `elem` s d) && not (e `elem` s c) =
+        (CGraph sneu r, True)
+    | otherwise = (CGraph s r, False)
+  where
+    sneu = except s c (e:s c)
+
+
+-- Functions ensuring the rules are applied exhaustively
+
+--computeCGraph :: TBox -> CGraph
+--computeCGraph TBox g:gs cs rs
+--         = 
+--         
+--
+--
+--
+--
+--         
+--applyRules :: CGraph -> GCI -> CGraph
+--applyRules CGraph
