a2058f83b1febe388fc70c7a5d9dd86e84b8547f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2013-11-06 18:15:52 +0100

    Add experimental completion procedure
    Use a State monad to track which nodes and edges have changed.
    
    TODO: Add a test case.
    
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- test/Hazel/CompletionSpec.hs
+++ test/Hazel/CompletionSpec.hs
@@ -4,12 +4,18 @@ module Hazel.CompletionSpec (spec)
        where
 
 import Prelude hiding (succ)
+import Control.Monad.State.Lazy ( runState
+                                , evalState
+                                )
 import Test.Hspec
 
 import Hazel.Core ( Concept (..)
                   , Role (..))
 import Hazel.Completion
 import Hazel.TestCases
+
+iS :: CState
+iS = CState [] []
 
 spec :: Spec
 spec = do
@@ -19,23 +25,23 @@ spec = do
       initS name `shouldBe` [name]
   describe "Completion Rules" $ do
     describe "CR1 with Top" $ do
-      let (CGraph n _, found) = (cr1 cr1_gci iG top, True)
+      let (CGraph n _, (CState ns _)) = (flip runState) iS $ cr1 cr1_gci iG top
       it "should find a new node" $
-        found `shouldBe` True
+        null ns `shouldBe` False
       it "should find Person and Top" $
         n name `shouldBe` [top, name]
     describe "CR1 with Person" $ do
-      let (CGraph n _, found) = (cr1 cr1_gci iG name, False)
+      let (CGraph n _, (CState ns _)) = (flip runState) iS $ cr1 cr1_gci iG name
       it "should not find a new node" $
-        found `shouldBe` False
+        null ns `shouldBe` True
       it "should have Person and Top" $
         n name `shouldBe` [name]
       it "should have allNodes" $
         n top `shouldBe` top:allNodes
     describe "CR2" $ do
-      let (CGraph n _, found) = (cr2 cr2_gci iG name, True)
+      let (CGraph n _, (CState ns _)) = (flip runState) iS $ cr2 cr2_gci iG name
       it "should find a new successor" $
-        found `shouldBe` True
+        null ns `shouldBe` False
       it "should find Person, and Human" $
         n (Name "Human") `shouldBe` [name, Name "Human"]
       it "should find allNodes" $
@@ -43,18 +49,18 @@ spec = do
       it "should find Dummy and Top" $
         n (Name "Dummy") `shouldBe` [Name "Dummy"]
     describe "CR3" $ do
-      let (CGraph _ n, found) = (cr3 cr3_gci iG name, True)
+      let (CGraph _ n, (CState _ rs)) = (flip runState) iS $ cr3 cr3_gci iG name
       it "should find a new role pair" $
-        found `shouldBe` True
+        null rs `shouldBe` False
       it "should find (Person, Person)" $
         n role `shouldBe` [(name, name)]
       it "should not find any pairs" $
         n (Role "marriedTo") `shouldBe` []
     describe "CR4" $ do
-      let cgraph3 = cr3 cr3_gci iG name
-          (CGraph n _, found) = (cr4 cr4_gci cgraph3 (name, name), True)
+      let cgraph3 = (flip evalState) iS $ cr3 cr3_gci iG name
+          (CGraph n _, (CState ns _ )) = (flip runState) iS $ cr4 cr4_gci cgraph3 (name, name)
       it "should find a new successor" $
-        found `shouldBe` True
+        null ns `shouldBe` False
       it "should find Person, and Father" $
         n (Name "Father") `shouldBe` [name, Name "Father"]
       it "should find allNodes" $
