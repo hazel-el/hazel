0b859c7b02f4bcf19063624eee8c53a8915dcd62
Felix <felix@tcs.inf.tu-dresden.de>
2013-11-06 16:04:58 +0100

    First function in CGraph now maps labels to nodes, not vice versa

--- Hazel/Completion.hs
+++ Hazel/Completion.hs
@@ -20,12 +20,13 @@
 module Hazel.Completion
        where
 
+import Data.List
 import Hazel.Core
 
 data CGraph =
-    -- | Labelling function for nodes, and labelling function for edges
-    CGraph (Concept -> [Concept]) (Role -> [CEdge])
+    CGraph (Concept -> [Node]) (Role -> [CEdge])
 
+type Node = Concept
 type CEdge = (Concept, Concept)
 
 
@@ -42,55 +43,67 @@ except f x' y' x
   | x == x' = y'
   | otherwise = f x
 
-initGraph :: CGraph
+initGraph :: [Node] -> CGraph
 -- ^ Initialization of the completion graph
-initGraph =
-    CGraph s r
+initGraph allNodes =
+    CGraph n r
   where
-    s Top = [Top]
-    s (Name n) = [Name n, Top]
-    s (Dummy n) = [Dummy n, Top]
-    s _ = error "Complex concepts are not valid nodes in CGraph"
+    n Top = nub $ Top : allNodes
+    n x@(Name _) = [x]
+    n x@(Dummy _) = [x]
+    n _ = error "Complex concepts are not valid nodes in CGraph"
     r _ = []
 
 -- Functions applying Completion Rules
 
-cr1 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
-cr2 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
-cr3 :: GCI -> CGraph -> Concept -> (CGraph, Bool)
-cr4 :: GCI -> CGraph -> Concept -> Concept -> (CGraph, Bool)
+cr1 :: GCI -> CGraph -> Concept -> CGraph
+cr2 :: GCI -> CGraph -> Concept -> CGraph
+cr3 :: GCI -> CGraph -> Concept -> CGraph
+cr4 :: GCI -> CGraph -> (Concept, Concept) -> CGraph
 
-cr1 (Subclass c' d) (CGraph s r) c
-    | (c' `elem` s c) && (d `notElem` s c) = (CGraph s' r, True)
-    | otherwise = (CGraph s r, False)
+cr1 (Subclass c' d) (CGraph n r) c
+    | (c `notElem` n d) && (c `elem` n c') = CGraph n' r
+    | otherwise = CGraph n r
   where
-    s' = except s c (d:s c)
+    n' = except n d (c:n d)
 
-cr2 (Subclass (And c1 c2) d) (CGraph s r) c
-    | (c1 `elem` s c) && (c2 `elem` s c) && (d `notElem` s c) =
-        (CGraph s' r, True)
-    | otherwise = (CGraph s r, False)
+cr2 (Subclass (And c1 c2) d) (CGraph n r) c
+    | (c `elem` n c1) && (c `elem` n c2) && (c `notElem` n d) =
+        CGraph n' r
+    | otherwise = CGraph n r
   where
-    s' = except s c (d:s c)
+    n' = except n d (c:n d)
 cr2 _ _ _ = error "Application of Rule CR2 not possible"
 
-cr3 (Subclass c' (Exists role d)) (CGraph s r) c
-    | (c' `elem` s c) && ((c, d) `notElem` r role) =
-        (CGraph s r', True)
-    | otherwise = (CGraph s r, False)
+cr3 (Subclass c' (Exists role d)) (CGraph n r) c
+    | (c `elem` n c') && ((c, d) `notElem` r role) =
+        CGraph n r'
+    | otherwise = CGraph n r
   where
     r' = except r role ((c, d):r role)
 cr3 _ _ _ = error "Application of Rule CR3 not possible"
 
-cr4 (Subclass (Exists role d') e) (CGraph s r) c d
-    | ((c, d) `elem` r role) && (d' `elem` s d) && (e `notElem` s c) =
-        (CGraph s' r, True)
-    | otherwise = (CGraph s r, False)
+cr4 (Subclass (Exists role d') e) (CGraph n r) (c, d)
+    | ((c, d) `elem` r role) && (d `elem` n d') && (c `notElem` n e) =
+        CGraph n' r
+    | otherwise = CGraph n r
   where
-    s' = except s c (e:s c)
-cr4 _ _ _ _ = error "Application of Rule CR4 not possible"
+    n' = except n e (c:n e)
+cr4 _ _ _ = error "Application of Rule CR4 not possible"
+
 
 -- Functions ensuring the rules are applied exhaustively
+
+--cr1 :: GCI -> CGraph -> Concept -> CGraph
+--cr2 :: GCI -> CGraph -> Concept -> CGraph
+--cr3 :: GCI -> CGraph -> Concept -> CGraph
+--cr4 :: GCI -> CGraph -> (Concept, Concept) -> CGraph
+--
+--iterateNodes :: GCI -> CGraph -> CGraph
+-----iterateNodes gci@(Subclass (Name c) (Name d)) cGraph = foldr (cr2 gci) cGraph allNodes
+-----iterateNodes gci@(Subclass (Name c) (Name d)) cGraph = foldr (cr3 gci) cGraph allNodes
+--iterateNodes gci@(Subclass (Exists role d') e) (CGraph s r) = foldr (cr4 gci) cGraph (r role)
+--iterateNodes gci@(Subclass c d) cGraph = foldr (cr1 gci) cGraph allNodes
 
 --computeCGraph :: TBox -> CGraph
 --computeCGraph TBox g:gs cs rs
--- test/Hazel/CompletionSpec.hs
+++ test/Hazel/CompletionSpec.hs
@@ -15,47 +15,48 @@ spec :: Spec
 spec = do
   describe "Completion Graph Initialization" $
     it "initializes the completion graph" $ do
-      let CGraph initS _ = initGraph
-      initS name `shouldBe` [name, top]
+      let CGraph initS _ = iG
+      initS name `shouldBe` [name]
   describe "Completion Rules" $ do
     describe "CR1 with Top" $ do
-      let (CGraph succ _, found) = cr1 cr1_gci initGraph top
-      it "should find a new successor" $
+      let (CGraph n _, found) = (cr1 cr1_gci iG top, True)
+      it "should find a new node" $
         found `shouldBe` True
       it "should find Person and Top" $
-        succ top `shouldBe` [name, top]
+        n name `shouldBe` [top, name]
     describe "CR1 with Person" $ do
-      let (CGraph succ _, found) = cr1 cr1_gci initGraph name
-      it "should not find a new successor" $
+      let (CGraph n _, found) = (cr1 cr1_gci iG name, False)
+      it "should not find a new node" $
         found `shouldBe` False
       it "should have Person and Top" $
-        succ name `shouldBe` [name, top]
-      it "should have Top" $
-        succ top `shouldBe` [top]
+        n name `shouldBe` [name]
+      it "should have allNodes" $
+        n top `shouldBe` top:allNodes
     describe "CR2" $ do
-      let (CGraph succ _, found) = cr2 cr2_gci initGraph name
+      let (CGraph n _, found) = (cr2 cr2_gci iG name, True)
       it "should find a new successor" $
         found `shouldBe` True
-      it "sholud find Human, Person, and Top" $
-        succ name `shouldBe` [Name "Human", name, top]
-      it "should find Top" $
-        succ top `shouldBe` [top]
+      it "should find Person, and Human" $
+        n (Name "Human") `shouldBe` [name, Name "Human"]
+      it "should find allNodes" $
+        n top `shouldBe` top:allNodes
       it "should find Dummy and Top" $
-        succ (Name "Dummy") `shouldBe` [(Name "Dummy"), top]
+        n (Name "Dummy") `shouldBe` [Name "Dummy"]
     describe "CR3" $ do
-      let (CGraph _ succ, found) = cr3 cr3_gci initGraph name
+      let (CGraph _ n, found) = (cr3 cr3_gci iG name, True)
       it "should find a new role pair" $
         found `shouldBe` True
       it "should find (Person, Person)" $
-        succ role `shouldBe` [(name, name)]
+        n role `shouldBe` [(name, name)]
       it "should not find any pairs" $
-        succ (Role "marriedTo") `shouldBe` []
+        n (Role "marriedTo") `shouldBe` []
     describe "CR4" $ do
-      let cgraph3 = fst $ cr3 cr3_gci initGraph name
-          (CGraph succ _, found) = cr4 cr4_gci cgraph3 name name
+      let cgraph3 = cr3 cr3_gci iG name
+          (CGraph n _, found) = (cr4 cr4_gci cgraph3 (name, name), True)
       it "should find a new successor" $
         found `shouldBe` True
-      it "should find Father, Person, and Top" $
-        succ name `shouldBe` [Name "Father", name, top]
-      it "should find Top" $
-        succ top `shouldBe` [top]
+      it "should find Person, and Father" $
+        n (Name "Father") `shouldBe` [name, Name "Father"]
+      it "should find allNodes" $
+        n top `shouldBe` top:allNodes
+  where iG = initGraph allNodes
