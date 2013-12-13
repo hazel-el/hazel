b14b3aa8b56c31f8f648eb13352228ebff354f84
Felix Distel <felix.distel@tu-dresden.de>
2013-12-13 15:22:23 +0100

    Test Case fixed, small edge case added

--- test/Hazel/CompletionSpec.hs
+++ test/Hazel/CompletionSpec.hs
@@ -4,6 +4,7 @@ module Hazel.CompletionSpec (spec)
        where
 
 import Data.List (intersect)
+import Data.Set (fromList)
 
 import Prelude hiding (succ)
 import Control.Monad.State.Lazy ( runState
@@ -27,13 +28,13 @@ spec = do
       initS name `shouldBe` [name]
   describe "Completion Rules" $ do
     describe "CR1 with Top" $ do
-      let (CGraph n _, CState ns _) = (flip runState) iS $ cr1 cr1Gci iG top
+      let (CGraph n _, CState ns _) = flip runState iS $ cr1 cr1Gci iG top
       it "should find a new node" $
         null ns `shouldBe` False
       it "should find Person and Top" $
         n name `shouldBe` [top, name]
     describe "CR1 with Person" $ do
-      let (CGraph n _, CState ns _) = (flip runState) iS $ cr1 cr1Gci iG name
+      let (CGraph n _, CState ns _) = flip runState iS $ cr1 cr1Gci iG name
       it "should not find a new node" $
         null ns `shouldBe` True
       it "should have Person and Top" $
@@ -41,7 +42,7 @@ spec = do
       it "should have allNodes" $
         n top `shouldBe` top:allNodes
     describe "CR2" $ do
-      let (CGraph n _, CState ns _) = (flip runState) iS $ cr2 cr2Gci iG name
+      let (CGraph n _, CState ns _) = flip runState iS $ cr2 cr2Gci iG name
       it "should find a new successor" $
         null ns `shouldBe` False
       it "should find Person, and Human" $
@@ -51,7 +52,7 @@ spec = do
       it "should find Dummy and Top" $
         n (Name "Dummy") `shouldBe` [Name "Dummy"]
     describe "CR3" $ do
-      let (CGraph _ n, CState _ rs) = (flip runState) iS $ cr3 cr3Gci iG name
+      let (CGraph _ n, CState _ rs) = flip runState iS $ cr3 cr3Gci iG name
       it "should find a new role pair" $
         null rs `shouldBe` False
       it "should find (Person, Person)" $
@@ -59,8 +60,8 @@ spec = do
       it "should not find any pairs" $
         n (Role "marriedTo") `shouldBe` []
     describe "CR4" $ do
-      let cgraph3 = (flip evalState) iS $ cr3 cr3Gci iG name
-          (CGraph n _, CState ns _ ) = (flip runState) iS $ cr4 cr4Gci cgraph3 (name, name)
+      let cgraph3 = flip evalState iS $ cr3 cr3Gci iG name
+          (CGraph n _, CState ns _ ) = flip runState iS $ cr4 cr4Gci cgraph3 (name, name)
       it "should find a new successor" $
         null ns `shouldBe` False
       it "should find Person, and Father" $
@@ -70,11 +71,16 @@ spec = do
     describe "Exercise 38" $ do
       let cg38 = complete normalized38
       it "Subsumees of A should be A" $
-        getNodes cg38 a `intersect` [a, b, c, d] `shouldBe` [a]
+        fromList (getNodes cg38 a `intersect` [a, b, c, d]) `shouldBe` fromList [a]
       it "Subsumees of B should be A, B, C" $
-        getNodes cg38 b `intersect` [a, b, c, d] `shouldBe` [a, b, c]
+        fromList (getNodes cg38 b `intersect` [a, b, c, d]) `shouldBe` fromList [a, b, c]
       it "Subsumees of C should be A, C" $
-        getNodes cg38 b `intersect` [a, b, c, d] `shouldBe` [a, c]
+        fromList (getNodes cg38 c `intersect` [a, b, c, d]) `shouldBe` fromList [a, c]
       it "Subsumees of D should be A, C, D" $
-        getNodes cg38 b `intersect` [a, b, c, d] `shouldBe` [a, c, d]
+        fromList (getNodes cg38 d `intersect` [a, b, c, d]) `shouldBe` fromList [a, c, d]
+    describe "Top Test" $ do
+      let compTop = iterateGCI (initGraph [a, b]) topTest
+          (_, CState cnodes _) = runState compTop emptyState
+      it "all nodes including top should have changed" $
+          fromList cnodes `shouldBe` fromList [a, b, Top]
   where iG = initGraph allNodes
