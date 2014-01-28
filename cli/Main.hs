472fdf8027abd87185498f56a77eda5d2fa95b86
Felix Distel <felix.distel@tu-dresden.de>
2014-01-28 23:44:31 +0100

    command line interface now parses, normalizes and performs completion

--- cli/Main.hs
+++ cli/Main.hs
@@ -3,14 +3,26 @@ module Main (main) where
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
+import Data.Set (elemAt)
+
 import Hazel.Parser (parseFile)
+import Hazel.Conversion (extractGCIs)
+import Hazel.Core (TBox (..))
+import Hazel.Normalize (normalize)
+import Hazel.Completion ( complete
+                        , CGraph (..)
+                        )
 
 parse :: FilePath -> IO ()
 parse file = do
   result <- parseFile file
   case result of
     (Left err) -> putStrLn $ "failed parsing `" ++ file ++ "': `" ++ err ++ "'."
-    (Right _) -> putStrLn $ "successfully parsed `" ++ file ++ "'."
+    (Right o) -> print $ getNodes cg $ elemAt 1 names
+      where
+        t = normalize $ extractGCIs o
+        TBox _ names _ = t
+        cg = complete t
 
 tryParse :: String -> IO ()
 tryParse file = do
