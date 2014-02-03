5f540fd8d8f61bbe19f6de353d8fb290a998b931
Felix Distel <felix.distel@tu-dresden.de>
2014-02-03 17:04:37 +0100

    Renamed Conversion to Wrapper

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,29 +1,27 @@
 module Main (main) where
 
+import Data.Set (toList)
 import System.Directory (doesFileExist)
 import System.Environment (getArgs)
 
-import Data.Set (toList)
-
-import Hazel.Parser (parseFile)
-import Hazel.Conversion (extractGCIs)
 import Hazel.Core (TBox (..))
-import Hazel.Normalize (normalize)
-import Hazel.Completion ( complete
-                        , CGraph (..)
+import Hazel.Completion ( CGraph (..)
+                        , complete
                         )
+import Hazel.Wrapper (extractGCIs)
+import Hazel.Normalize (normalize)
+import Hazel.Parser (parseFile)
 
 parse :: FilePath -> IO ()
 parse file = do
   result <- parseFile file
   case result of
     (Left err) -> putStrLn $ "failed parsing `" ++ file ++ "': `" ++ err ++ "'."
-    (Right o) -> print $ zip ns $ map (getNodes cg) ns
-      where
-        ns = toList names
-        t = normalize $ extractGCIs o
-        TBox _ names _ = t
-        cg = complete t
+    (Right ast) -> do
+      putStrLn $ "successfully parsed `" ++ file ++ "'."
+      let tbox@(TBox _ names _) = normalize $ extractGCIs ast
+          cg = complete tbox
+      print . getNodes cg $ head . toList $ names
 
 tryParse :: String -> IO ()
 tryParse file = do
